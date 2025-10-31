71f59026b070e445a7a7466b22b0127e91c641f9
Levent Esen <leventesen12@hotmail.com>
2025-10-31 22:51:17 +0300

    Remove mat-button

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   imports: [
-    CommonModule,
-    MatButtonModule
+    CommonModule
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
